14f397b94b0614cc0c7eaacbaddbd00eae4e5e3c
yaoweihao <yaoweihao@jd.com>
2018-07-04 20:55:52 +0800

    改成maven

---
+++ src/main/java/com/showtime/ioc/BeanDefinition.java
@@ -0,0 +1,13 @@
+package com.showtime.ioc;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @description:
+ * @author: yaoweihao
+ * @date: 2018/7/3
+ * @time: 16:54
+ * @modified by:
+ */
+public class BeanDefinition {
+}
---
+++ src/main/java/com/showtime/ioc/BeanFactory.java
@@ -0,0 +1,13 @@
+package com.showtime.ioc;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @description:
+ * @author: yaoweihao
+ * @date: 2018/7/3
+ * @time: 16:56
+ * @modified by:
+ */
+public class BeanFactory {
+}
