4ff587a5b35d60ceb359f5a6bb5c421a4a6d5a5f
show4268time <show4268time@163.com>
2018-07-28 00:21:45 +0800

    xmlBeanDefinitionReaderTest

--- src/main/java/com/showtime/ioc/xml/XmlBeanDefinitionReader.java
+++ src/main/java/com/showtime/ioc/xml/XmlBeanDefinitionReader.java
@@ -1,8 +1,13 @@
 package com.showtime.ioc.xml;
 
 import com.showtime.ioc.AbstractBeanDefinitionReader;
+import com.showtime.ioc.BeanDefinition;
+import com.showtime.ioc.PropertyValue;
 import com.showtime.ioc.io.ResourceLoader;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -18,17 +23,62 @@ import java.io.InputStream;
  * @modified by:
  */
 public class XmlBeanDefinitionReader extends AbstractBeanDefinitionReader {
+
     public XmlBeanDefinitionReader(ResourceLoader resourceLoader){
         super(resourceLoader);
     }
+
     @Override
     public void loadBeanDefinitions(String location) throws Exception {
         InputStream inputStream = getResourceLoader().getResource(location).getInputStream();
+        doLoadBeanDefinitions(inputStream);
     }
 
     protected void doLoadBeanDefinitions(InputStream inputStream) throws Exception{
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = documentBuilderFactory.newDocumentBuilder();
-        Document doc = docBuilder.parse(inputStream);
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(inputStream);
+        //解析bean
+        registerBeanDefinitions(document);
+        inputStream.close();
+    }
+
+    public void registerBeanDefinitions(Document document){
+        Element root = document.getDocumentElement();
+        parseBeanDefinitions(root);
+    }
+
+    protected void parseBeanDefinitions(Element root){
+        NodeList nodeList = root.getChildNodes();
+        for(int i = 0; i < nodeList.getLength(); i++){
+            Node node = nodeList.item(i);
+            if(node instanceof Element){
+                Element element = (Element) node;
+                processBeanDefinition(element);
+            }
+        }
+    }
+
+    protected void processBeanDefinition(Element element){
+        String name = element.getAttribute("name");
+        String className = element.getAttribute("class");
+        BeanDefinition beanDefinition = new BeanDefinition();
+        processProperty(element, beanDefinition);
+        beanDefinition.setClassName(className);
+        getRegistry().put(name, beanDefinition);
+    }
+
+    private void processProperty(Element element, BeanDefinition beanDefinition){
+        NodeList propertyNodeList = element.getElementsByTagName("property");
+        for(int i = 0; i < propertyNodeList.getLength(); i++){
+            Node node = propertyNodeList.item(i);
+            if (node instanceof Element){
+                Element propertyElement = (Element) node;
+                String name = propertyElement.getAttribute("name");
+                Object value = propertyElement.getAttribute("value");
+                beanDefinition.getPropertyValues().addPropertyValue(new PropertyValue(name, value));
+            }
+        }
+
     }
 }
