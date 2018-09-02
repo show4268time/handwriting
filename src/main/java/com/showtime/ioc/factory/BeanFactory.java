e8f19de5d44a8becffbdcf4e644aa785f8063bad
show4268time <show4268time@163.com>
2018-09-02 22:50:14 +0800

    重新开始

--- src/main/java/com/showtime/ioc/factory/BeanFactory.java
+++
@@ -1,9 +0,0 @@
-package com.showtime.ioc.factory;
-
-import com.showtime.ioc.BeanDefinition;
-
-public interface BeanFactory {
-    Object getBean(String name) throws Exception;
-
-    void registerBeanDefinition(String name, BeanDefinition beanDefinition) throws Exception;
-}
